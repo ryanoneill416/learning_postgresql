863b7b1d58db153c5ca1bbb5321c9aa7f920b650
Ryan O'Neill <ryanoneill416@gmail.com>
2022-08-25 20:46:46 +0000

    begin building db to practice crud

--- sql-crud.py
+++ sql-crud.py
@@ -11,14 +11,12 @@ base = declarative_base()
 
 
 # create a class-based model for the "Programmer" table
-class Programmer(base):
-    __tablename__ = "Programmer"
+class FavPlace(base):
+    __tablename__ = "FavouritePlaces"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    gender = Column(String)
-    nationality = Column(String)
-    famous_for = Column(String)
+    country = Column(String)
+    nat_language = Column(String)
+    population = Column(Integer)
 
 
 # instead of connecting to db directly, we ask for a session
@@ -32,83 +30,98 @@ base.metadata.create_all(db)
 
 
 # creating records on our programmer table
-ada_lovelace =  Programmer(
-    first_name="Ada",
-    last_name="Lovelace",
-    gender="F",
-    nationality="British",
-    famous_for="First Programmer"
+my_house = FavPlace(
+    country="Ireland",
+    nat_language="English",
+    population=3
 )
 
-alan_turing =  Programmer(
-    first_name="Alan",
-    last_name="Turing",
-    gender="M",
-    nationality="British",
-    famous_for="Modern Computing"
+paleokastritsa = FavPlace(
+    country="Greece",
+    nat_language="Greek",
+    population=1242078
 )
 
-grace_hopper =  Programmer(
-    first_name="Grace",
-    last_name="Hopper",
-    gender="M",
-    nationality="American",
-    famous_for="COBOL Language"
+budapest = FavPlace(
+    country="Hungary",
+    nat_language="Hungarian",
+    population=4234742
 )
 
-margaret_hamilton =  Programmer(
-    first_name="Margaret",
-    last_name="Hamilton",
-    gender="F",
-    nationality="American",
-    famous_for="Apollo 11"
+leeds = FavPlace(
+    country="United Kingdom",
+    nat_language="English",
+    population=5384243
 )
 
-bill_gates =  Programmer(
-    first_name="Bill",
-    last_name="Gates",
-    gender="M",
-    nationality="American",
-    famous_for="Microsoft"
-)
-
-tim_berners_lee =  Programmer(
-    first_name="Tim",
-    last_name="Berners-Lee",
-    gender="M",
-    nationality="British",
-    famous_for="World Wide Web"
-)
-
-ryan_oneill =  Programmer(
-    first_name="Ryan",
-    last_name="O'Neill",
-    gender="M",
-    nationality="Irish",
-    famous_for="Being sick B)"
+morroccoy = FavPlace(
+    country="Venezuela",
+    nat_language="Spanish",
+    population=230076
 )
 
 
 # add each instance of our programmers to our session
-session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margaret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
+session.add(my_house)
+session.add(paleokastritsa)
+session.add(budapest)
+session.add(leeds)
+session.add(morroccoy)
 
 
 # commit our session to the database
-session.commit()
+# session.commit()
+
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=8).first()
+# programmer.famous_for = "World President"
+
+
+# # updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#         session.commit()
+
+
+# deleting a single record
+# fname = input("Enter first name: ")
+# lname = input("Enter last name: ")
+# programmer = session.query(Programmer).filter_by(first_name = fname, last_name= lname).first()
+# # defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Selected record deleted successfully")
+#     else:
+#         print("Selected record not deleted")
+# else:
+#     print("No matching records found")
+
+
+# delete multiple records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
+
 
 # query the database to find all Programmers
-programmers = session.query(Programmer)
-for programmer in programmers:
+favplaces = session.query(FavPlace)
+for place in favplaces:
     print(
-        programmer.id,
-        programmer.first_name + " " + programmer.last_name,
-        programmer.gender,
-        programmer.nationality,
-        programmer.famous_for,
-        sep = " | "
+        place.id,
+        place.country,
+        place.nat_language,
+        place.population,
+        sep=" | "
     )
